1385a270f4f349806e8fc52b1217c03f7e806c2e
freyamade <frybrdrck@gmail.com>
2019-03-13 22:01:05 +0000

    Really simple basic version done
    This is enough for me to be honest, it does what I want it do to
    albeit naively, so I'm happy!

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,35 @@
 import kotlin.browser.document
+import kotlin.browser.window
+import org.w3c.dom.Element
+import org.w3c.dom.HTMLElement
 
 fun main() {
-    document.body?.style?.border = "5px solid #0046cc"
+    // Run the function after a second to ensure the page has fully loaded
+    var location = window.location.pathname.substring(1).split("/")
+    if (location.size == 2) {
+        window.setTimeout(
+            fun() { render() },
+            1000
+        )
+    }
+}
+
+// Actually handle the rendering functionality
+fun render() {
+    // Try to find the tease sha
+    var commitTeaseShaCollection = document.getElementsByClassName("commit-tease-sha")
+    if (commitTeaseShaCollection.length == 1) {
+        // We've found the tease sha element, now get the first 6 characters of it
+        var commitTeaseShaElement = commitTeaseShaCollection.item(0) as HTMLElement
+        var colour = commitTeaseShaElement.innerText.substring(0, 6)
+
+        // Attempt to find the set of social buttons at the top right
+        var buttonContainerCollection = document.getElementsByClassName("pagehead-actions")
+        if (buttonContainerCollection.length == 1) {
+            // Create our colour button and add it to the existing list
+            var elementString = "<li><p class=\"btn btn-sm btn-with-count\">Colour</p><p class=\"social-count js-social-count\" style=\"background-color:#$colour !important;\">&nbsp;</p></li>"
+            var buttonContainer = buttonContainerCollection.item(0) as Element
+            buttonContainer.innerHTML = elementString + buttonContainer.innerHTML
+        }
+    }
 }
