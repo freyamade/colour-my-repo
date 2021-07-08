212a2d917b648de5a9f4e08f68d625d891d0c7c7
freyamade <frybrdrck@gmail.com>
2021-07-08 22:38:08 +0100

    Setting up for actual deployment to the browser stores

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@
 # First build the kotlin files
 ./gradlew runDceKotlinJs
 # Then uglify them into the dist folder
-uglifyjs --compress --mangle -o dist/content.js -- build/kotlin-js-min/main/colour-my-repo.js
-uglifyjs --compress --mangle -o dist/kotlin.js -- build/kotlin-js-min/main/kotlin.js
+npx uglifyjs --compress --mangle -o dist/content.js -- build/kotlin-js-min/main/colour-my-repo.js
+npx uglifyjs --compress --mangle -o dist/kotlin.js -- build/kotlin-js-min/main/kotlin.js
